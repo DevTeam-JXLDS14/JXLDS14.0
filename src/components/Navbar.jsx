8143f0fc915c7b390120b69a48ddfd7a3aa996ca
Darshana-NWN <en108937@foe.sjp.ac.lk>
2025-11-25 01:20:15 +0530

    Add new logo image to the project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import './Navbar.css';
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import "./Navbar.css";
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -13,7 +13,11 @@ function Navbar() {
     <nav className="navbar">
       <div className="nav-container">
         <Link to="/" className="nav-logo">
-          JXLDS 14.0
+          <img
+            src="/images/logo.png"
+            alt="JXLDS 14.0"
+            className="nav-logo-img"
+          />
         </Link>
 
         <button className="menu-toggle" onClick={toggleMenu}>
@@ -22,29 +26,49 @@ function Navbar() {
           <span></span>
         </button>
 
-        <ul className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
+        <ul className={`nav-menu ${isMenuOpen ? "active" : ""}`}>
           <li className="nav-item">
-            <Link to="/" className="nav-link" onClick={() => setIsMenuOpen(false)}>
+            <Link
+              to="/"
+              className="nav-link"
+              onClick={() => setIsMenuOpen(false)}
+            >
               Home
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/register" className="nav-link" onClick={() => setIsMenuOpen(false)}>
+            <Link
+              to="/register"
+              className="nav-link"
+              onClick={() => setIsMenuOpen(false)}
+            >
               Register
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/organizing-committee" className="nav-link" onClick={() => setIsMenuOpen(false)}>
+            <Link
+              to="/organizing-committee"
+              className="nav-link"
+              onClick={() => setIsMenuOpen(false)}
+            >
               OC Team
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/facilitators" className="nav-link" onClick={() => setIsMenuOpen(false)}>
+            <Link
+              to="/facilitators"
+              className="nav-link"
+              onClick={() => setIsMenuOpen(false)}
+            >
               Facilitators
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/store" className="nav-link" onClick={() => setIsMenuOpen(false)}>
+            <Link
+              to="/store"
+              className="nav-link"
+              onClick={() => setIsMenuOpen(false)}
+            >
               Store
             </Link>
           </li>
